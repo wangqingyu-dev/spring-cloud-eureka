44fa789a78741a41336ba69c5069f7644b1a44c0
wangqingyu-dev <62085025+wangqingyu-dev@users.noreply.github.com>
2021-01-12 09:41:48 +0800

    Add files via upload

---
+++ springcloud-ms-eureka/src/main/java/com/swapping/springcloudmseureka/SpringcloudMsEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.swapping.springcloudmseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringcloudMsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudMsEurekaApplication.class, args);
+    }
+
+}
